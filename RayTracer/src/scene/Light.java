a0023da62056d6971f61bc557248afba63eb3c91
Styrof <hectoral_nba@hotmail.com>
2015-11-20 10:02:29 +0100

    Light

---
+++ RayTracer/src/scene/Light.java
@@ -0,0 +1,37 @@
+package scene;
+
+import javax.vecmath.*;
+
+public class Light {
+	private Vector3d position;
+	private Vector3d direction;
+
+	public Light (Vector3d position, Vector3d direction) {
+		this.position = position;
+		this.direction = direction;
+	}
+	
+	public Vector3d getPosition() {
+		return position;
+	}
+
+	public Vector3d getDirection() {
+		return direction;
+	}
+
+	public void setPosition(Vector3d position) {
+		this.position = position;
+	}
+	
+	public void setDirection(Vector3d direction) {
+		this.direction = direction;
+	}
+
+	public void transform(Matrix4d transformationMatrix) {
+		Vector4d aux = new Vector4d(position.x, position.y, position
+				.z, 1);
+		aux = Util.MultiplyVectorAndMatrix(transformationMatrix, aux);
+		position.set(aux.x, aux.y, aux.z);
+	}
+	
+}
